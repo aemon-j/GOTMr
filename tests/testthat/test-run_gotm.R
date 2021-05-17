558393b7f16f4633b86971fb025d0442873a405c
Tadhg Moore <tadhgm@vt.edu>
2021-05-17 12:29:43 -0400

    Adjusted test to work with corrected verbose

--- tests/testthat/test-run_gotm.R
+++ tests/testthat/test-run_gotm.R
@@ -3,7 +3,7 @@ test_that("running gotm simulation", {
   sim_folder <- system.file('extdata', package = 'GOTMr')
   status = run_gotm(sim_folder, verbose = TRUE)
 
-  expect_true(is.character(status))
+  expect_true(status == 0)
 
   expect_true(file.exists(file.path(sim_folder, 'output/output.nc')))
 
@@ -16,5 +16,4 @@ test_that("running gotm simulation - verbose = FALSE", {
   expect_true(status == 0)
 
   expect_true(file.exists(file.path(sim_folder, 'output/output.nc')))
-
 })
